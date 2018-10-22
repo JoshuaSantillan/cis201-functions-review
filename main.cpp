2eb24ebb5423febbfb325cd64db5d989e3fddfe5
Joshua Santillan <santj96@gmail.com>
2018-10-22 08:29:47 -0700

    late update

--- main.cpp
+++ main.cpp
@@ -1,12 +1,27 @@
 #include<iostream>
-
-
+#include<string>
 using namespace std;
 
 
+int test(string a);
 int main()
 {
-    cout << "Hello World"<< endl;
-
+    string a = "The Quick Brown Fox Jumped Over The Lazy Dog";
+    cout << test(a) << endl;  
     return 0;
+}
+
+
+int test(string a)
+{
+    int count = 0;
+    for (int i = 0; i<a.length();i++)
+        {
+            char find_a = a.at(i);
+            if (find_a == 'a'|| find_a == 'A')
+            {
+                count++;
+            }
+        }
+    return count;
 }
